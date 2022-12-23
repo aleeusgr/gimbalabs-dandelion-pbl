5ba9d904042d2112489ba5ebd01fca39661b8ac5
50^2 <alexeusgr@gmail.com>
2022-12-23 09:06:25 +0200

    what is the bounty asset?

--- scripts/commit-to-bounty-parameters.sh
+++ scripts/commit-to-bounty-parameters.sh
@@ -25,8 +25,6 @@ BOUNTY_DATUM="$ppbl/ppbl-course-02/project-303/bounty-treasury-escrow/datum-and-
 # $path/cardano-cli query utxo --testnet-magic 1 --address $TREASURY_ADDR
 CONTRACT_TXIN=c09aed80866bd5387ff221e0ca8458ef953b573e39ca682cf83110ca73ebb355#0
 LOVELACE_AT_TREASURY=1500000
-
-#read BOUNTY_TOKENS_AT_TREASURY
 BOUNTY_TOKENS_AT_TREASURY=9999990
 
 # $path/cardano-cli query utxo --testnet-magic 1 --address $CONTRIBUTOR
@@ -36,12 +34,12 @@ COLLATERAL=db3fa2dcb77e1a935705fd85bf8a46ee796a3cec447b6343c62680eebea92f94#0
 TXIN1=4f8962fc94718bd516e22d8ad72bdd25f14ad62ecbdc3ad0159be4db7151e6dc#0
 #echo "What is the Asset ID of your Contributor Token?"
 #read CONTRIBUTOR_ASSET
-CONTRIBUTOR_ASSET=9a14207c494a43dc2d30eebda683b1dcf2c8b42c48dc6195c4b5e948.636f44534554
+CONTRIBUTOR_ASSET="9a14207c494a43dc2d30eebda683b1dcf2c8b42c48dc6195c4b5e948.636f44534554"
 #echo "Specify a TXIN with with additional lovelace (for tx fees):"
 TXIN2=db3fa2dcb77e1a935705fd85bf8a46ee796a3cec447b6343c62680eebea92f94#0
 # echo "Amount of lovelace in this bounty:"
 BOUNTY_LOVELACE=6500000
-# echo "Number of tgimbals in this bounty:"
+# echo "Number of DSET in this bounty:"
 BOUNTY_AMOUNT=10
 
 LOVELACE_BACK_TO_TREASURY=$(expr $LOVELACE_AT_TREASURY - $BOUNTY_LOVELACE)
