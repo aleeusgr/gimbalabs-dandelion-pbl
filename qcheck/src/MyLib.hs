bdcaa2602b0fd57e3852464a67c5cda696167699
50^2 <alexeusgr@gmail.com>
2023-01-05 16:34:05 +0200

    following a tutorial

--- qcheck/src/MyLib.hs
+++ qcheck/src/MyLib.hs
@@ -5,11 +5,20 @@ module MyLib (someFunc) where
 
 add' :: Int -> Int -> Int
 add' x y = x + y
---
+
 concat'' :: [Char] -> [Char] -> [Char]
 concat'' x y = x ++ y
+
+--
+split :: Eq a => a -> [a] -> [[a]]
+split _ [] = []
+split c xs = xs' : if null xs'' then [] else split c (tail xs'')
+    where xs' = takeWhile (/=c) xs
+          xs''= dropWhile (/=c) xs
+
 
 someFunc :: IO ()
 someFunc = do
   print $ add' 3 4
   print $ concat'' "3" "4"
+  print $ split '@' "pbv@dcc.fc.up.pt"
--- qcheck/test/Main.hs
+++ qcheck/test/Main.hs
@@ -5,5 +5,7 @@ import Test.QuickCheck
 prop_revapp :: [Int] -> [Int] -> Bool
 prop_revapp xs ys = reverse (xs++ys) == reverse ys ++ reverse xs
 
+
 main :: IO()
-main = quickCheck prop_revapp
+main = do
+  quickCheck prop_revapp
