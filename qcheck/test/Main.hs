4c0140dce15f793903e3c389e9800a01d0f8cb17
50^2 <alexeusgr@gmail.com>
2023-01-05 16:01:10 +0200

    test passes

--- qcheck/test/Main.hs
+++ qcheck/test/Main.hs
@@ -3,7 +3,7 @@ module Main (main) where
 import Test.QuickCheck
 
 prop_revapp :: [Int] -> [Int] -> Bool
-prop_revapp xs ys = reverse (xs++ys) == reverse xs ++ reverse ys
+prop_revapp xs ys = reverse (xs++ys) == reverse ys ++ reverse xs
 
 main :: IO()
 main = quickCheck prop_revapp
