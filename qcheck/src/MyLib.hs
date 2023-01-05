0a834b387e5e32e9e3992e575d1ba73d435346e1
50^2 <alexeusgr@gmail.com>
2023-01-05 14:55:03 +0200

    one morphism, two functions. property should hold in one, but not the other

--- qcheck/src/MyLib.hs
+++ qcheck/src/MyLib.hs
@@ -1,4 +1,15 @@
 module MyLib (someFunc) where
 
+--for all (a, b, c) strings
+--the concatenation of a, b and c always contains b
+
+add' :: Int -> Int -> Int
+add' x y = x + y
+--
+concat'' :: [Char] -> [Char] -> [Char]
+concat'' x y = x ++ y
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+  print $ add' 3 4
+  print $ concat'' "3" "4"
