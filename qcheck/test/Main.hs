0e556dcd5c292406876f7efca90983129fdb4179
50^2 <alexeusgr@gmail.com>
2023-01-05 17:18:39 +0200

    test isomorphism

--- qcheck/test/Main.hs
+++ qcheck/test/Main.hs
@@ -1,11 +1,22 @@
 module Main (main) where
 
 import Test.QuickCheck
+import Data.List (intercalate)
 
-prop_revapp :: [Int] -> [Int] -> Bool
-prop_revapp xs ys = reverse (xs++ys) == reverse ys ++ reverse xs
+split :: Char -> String -> [String]
+split _ [] = []
+split c xs = xs' : if null xs'' then [] else split c (tail xs'')
+    where xs' = takeWhile (/=c) xs
+          xs''= dropWhile (/=c) xs
 
+unsplit :: Char -> [String] -> String
+unsplit c = intercalate [c]
+
+-- show
+prop_split_inv c xs 
+    = let ys = split c xs in 
+      collect (length ys) $ unsplit c ys == xs
 
 main :: IO()
 main = do
-  quickCheck prop_revapp
+  quickCheck prop_split_inv
